9ac9f2f757256a2588749eb2b009ef224939293c
Chronic <tinywhitepecker420@proton.me>
2024-10-29 12:24:16 +0000

    Create restartnetwork.bat

---
+++ restartnetwork.bat
@@ -0,0 +1,15 @@
+@echo off
+echo Restarting Network Services...
+
+net stop netman
+net stop dhcp
+net stop dnscache
+net stop nlasvc
+
+net start netman
+net start dhcp
+net start dnscache
+net start nlasvc
+
+echo All services restarted successfully.
+pause
